351748f2b34f9301297de04e42c5a7cc886b9bd1
udayhacks <kanchanpallyudayuday85@gmail.com>
2021-12-29 12:34:23 +0530

    updated with the numerical topic

---
+++ python_3.10.1/numbers/numbers.py
@@ -0,0 +1,52 @@
+# In python the number are divided into :
+# THREEE types and they are 
+# int (integers) , float(all fractional values) ,complex (which are of real and imaginary numbers )
+#
+# ----> int  example : 1,2,3,4,5,66,67, ............
+#-------> float examples :1.2,.35.0 ,0.09889  ,......
+#-----------> complex examples: 2+i8 ,3+i9,6+L4 ,e, .......
+# ALL THE OPERATORS ARE NORMAL IN THE PYTHON 
+# 
+#""""" HEERE WE ARE USING THE PRINT FUNCTION TO GET OUTPUT IN THE SOURCE CODE """"
+# SYNTAX OF PRINT("") OR PRINT() {LOWERCASE CASE SENSITIVE}
+
+
+
+
+#Any operation on number with ----->init  with   inti  is always init 
+
+print ("welcome to the python programming ")
+print( "Addition of two numbers : ",10 + 122)
+print( "Subtraction  of two numbers : ",10 - 122)
+print( "Multipication of two numbers : ",10 * 122)
+print( "Division  of two numbers : ",10 / 122)
+print( "Reminder of two numbers : ",10 % 122)
+
+#Any operation on number with ----->float  with   float is always float 
+
+print( "Addition of two numbers : ",10.3 + 12.2)
+print( "Subtraction  of two numbers : ",10.8 - 1.22)
+print( "Multipication of two numbers : ",10.0 * 122)
+print( "Division  of two numbers : ",10.0 / 1.22)
+print( "Reminder of two numbers : ",10.0 % 12.2)
+
+#Any operation on number with ----->float/int  with   int/float is always float  
+print( "Addition of two numbers : ",10.3 + 12.2)
+print( "Subtraction  of two numbers : ",10 - 1.22)
+print( "Multipication of two numbers : ",10.3 * 12)
+print( "Division  of two numbers : ",10 / 1.22)
+print( "Reminder of two numbers : ",10 % 12.2)
+
+
+
+# finally rarely complex number any operation complex numbers is always complex 
+
+
+print( "Addition of two numbers : ", complex(10,312) +complex( 12,2))
+print( "Subtraction  of two numbers : ",complex(10,120) - complex( 1,22))
+print( "Multipication of two numbers : ",complex(10,3) * complex (13, 12))
+print( "Division  of two numbers : ",complex(10,34) /complex( 1,22))
+
+
+## forgot to say  I'N THE CODE STARTING THE " # " ARE NOT EXCUTED IT IS  CALLED  single line commenting 
+# thse code with ---> """  3pairs  """ or '''3pairs '''' for MULTI LINE  COMMENTING 
